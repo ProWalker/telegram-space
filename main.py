7e78d8313deb95558d7f0e6210eb8946cb66eae0
ProWalker <provoker.1986@gmail.com>
2021-12-01 18:08:11 +0300

    placing constants in the file header. Remove telegram flood control handler.

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ import telegram
 import requests
 
 
-_images_path = 'images'
+load_dotenv()
+NASA_TOKEN = os.getenv('NASA_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+SCRIPT_DELAY = int(os.getenv('SCRIPT_DELAY'))
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+IMAGES_PATH = 'images'
 
 
 def get_image_extension_from_url(url):
@@ -24,7 +29,7 @@ def download_picture(url, image_name):
     response = requests.get(url)
     response.raise_for_status()
     image_extension = get_image_extension_from_url(url)
-    file_path = f'{_images_path}/{image_name}{image_extension}'
+    file_path = f'{IMAGES_PATH}/{image_name}{image_extension}'
 
     with open(file_path, 'wb') as file:
         file.write(response.content)
@@ -35,29 +40,23 @@ def bulk_download_picture(urls, image_name):
         download_picture(url, f'{image_name}{count}')
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    Path(_images_path).mkdir(parents=True, exist_ok=True)
-    telegram_token = os.getenv('TELEGRAM_TOKEN')
-    nasa_token = os.getenv('NASA_TOKEN')
-    tg_chat_id = os.getenv('TG_CHAT_ID')
-    script_delay = int(os.getenv('SCRIPT_DELAY'))
-    bot = telegram.Bot(token=telegram_token)
+def main():
+    Path(IMAGES_PATH).mkdir(parents=True, exist_ok=True)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     while True:
         spacex_images_links = fetch_spacex_last_launch()
-        nasa_apod_images_links = fetch_nasa_apod(nasa_token, 30)
-        nasa_epic_images_links = fetch_nasa_epic(nasa_token)
+        nasa_apod_images_links = fetch_nasa_apod(NASA_TOKEN, 30)
+        nasa_epic_images_links = fetch_nasa_epic(NASA_TOKEN)
         bulk_download_picture(spacex_images_links, 'spacex')
         bulk_download_picture(nasa_apod_images_links, 'apod')
         bulk_download_picture(nasa_epic_images_links, 'epic')
-        images = os.listdir(_images_path)
+        images = os.listdir(IMAGES_PATH)
         for image in images:
-            while True:
-                try:
-                    with open(f'{_images_path}/{image}', 'rb') as image_file:
-                        bot.send_document(chat_id=tg_chat_id, document=image_file)
-                    break
-                except telegram.error.RetryAfter as tgErrorRetryAfter:
-                    time.sleep(tgErrorRetryAfter.retry_after)
-        time.sleep(script_delay)
+            with open(f'{IMAGES_PATH}/{image}', 'rb') as image_file:
+                bot.send_document(chat_id=TG_CHAT_ID, document=image_file)
+            time.sleep(SCRIPT_DELAY)
+
+
+if __name__ == '__main__':
+    main()
 
